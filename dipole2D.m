e005dcc161da72475350d0f8fbfca5a133806831
Tianyou Xu <xut@TXMBP.local>
2016-04-17 23:09:06 +0100

    Gradient echo phase background removal technique
    Implementation of background field removal technique following Liu et
    al. 2011 “A novel background field removal method for MRI using
    projection onto dipole fields (PDF).”

---
+++ dipole2D.m
@@ -0,0 +1,17 @@
+function [kdPole] = dipole2D(X)
+% Make 3D FFT of dipole for use with quantitative susceptibility
+% mapping.
+
+xx=X(1);
+yy=X(2);
+kdPole=zeros([xx yy]);
+
+
+for y=1:yy;
+    for x=1:xx;
+        r=(x-(xx+1)/2)^2+(y-(yy+1)/2)^2;
+        kdPole(x,y)=1/3 -(x-(xx+1)/2)^2/r;
+    end;
+end;
+
+kdPole((xx)/2,(yy)/2)=0;
---
+++ pdf_method.m
@@ -0,0 +1,52 @@
+function [edf_liu, J]=pdf_method(data,mask,N)
+
+data=mask.*data;
+data(isnan(data))=0;
+P=data(:);
+
+%% Dipole kernel using the functional form in kspace
+
+C=dipole2D(size(data));
+c=conj(C);
+
+%% Masks and variables
+
+Mn=1-mask;
+W=ones(size(mask));
+%% Initialize variables and set iterations
+
+x=zeros(size(data));
+r=W.*mask.*data; 
+
+%% CG Alg
+
+F=fftshift(fftn(ifftshift(W.*r))); 
+d=Mn.*fftshift((ifftn(ifftshift(c.*F)))); 
+z0_ip=sum(reshape(conj(d).*d, [numel(data) 1]));
+
+TMP=[];
+for i=1:N;
+    Ad=mask.*W.*fftshift(ifftn(ifftshift(C.*fftshift(fftn(ifftshift(Mn.*d))))));
+    Ad_ip=sum(reshape(conj(Ad).*Ad,[numel(data) 1]));
+    alpha=(z0_ip)/(Ad_ip);
+    x=x+alpha*d;
+    r=r-alpha*Ad;
+    s=Mn.*fftshift(ifftn(ifftshift(c.*fftshift(fftn(ifftshift(.*W.*r)))))); 
+    
+    z0_new_ip=sum(reshape(conj(s).*s,[numel(data) 1]));
+    beta=(z0_new_ip)/(z0_ip);
+    z0_ip=z0_new_ip;
+    d=s+beta*d;
+    
+    tmp=norm(r(:));
+    
+    disp(['||r|| = ' num2str(tmp)])
+    TMP=[TMP;tmp];
+    
+end
+%%
+field_in= fftshift(ifftn(ifftshift(C.*fftshift(fftn(ifftshift(Mn.*x))))));
+edf_liu=real(mask.*(data - field_in));
+J= real(mask.* field_in);
+
+
